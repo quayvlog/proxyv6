d14f8bacd8e1bcf4522199eeba39eb4844b88b90
quayvlog <97423715+quayvlog@users.noreply.github.com>
2022-01-10 12:30:48 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
